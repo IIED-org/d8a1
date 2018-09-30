63025b6a5aa508be7b71f1cbcb4f09227854bf1f
Chris Brody <chris@cbrody.com>
2018-09-30 05:00:32 +0100

    Update module versions

--- docroot/modules/contrib/social_api/src/SocialApiDataHandler.php
+++ docroot/modules/contrib/social_api/src/SocialApiDataHandler.php
@@ -1,24 +1,15 @@
 <?php
 
-namespace Drupal\social_auth_facebook;
+namespace Drupal\social_api;
 
-use Facebook\PersistentData\PersistentDataInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 /**
  * Variables are written to and read from session via this class.
- *
- * By default, Facebook SDK uses native PHP sessions for storing data. We
- * implement Facebook\PersistentData\PersistentDataInterface using Symfony
- * Sessions so that Facebook SDK will use that instead of native PHP sessions.
- * Also SimpleFbConnect reads data from and writes data to session via this
- * class.
- *
- * @see https://developers.facebook.com/docs/php/PersistentDataInterface/5.0.0
  */
-class FacebookAuthPersistentDataHandler implements PersistentDataInterface {
+abstract class SocialApiDataHandler {
   protected $session;
-  protected $sessionPrefix = 'social_auth_facebook_';
+  protected $sessionPrefix;
 
   /**
    * Constructor.
@@ -31,14 +22,25 @@ class FacebookAuthPersistentDataHandler implements PersistentDataInterface {
   }
 
   /**
-   * {@inheritdoc}
+   * Gets a session variable by key.
+   *
+   * @param string $key
+   *   The session variable key.
+   *
+   * @return mixed
+   *   The session variable value.
    */
   public function get($key) {
     return $this->session->get($this->getSessionPrefix() . $key);
   }
 
   /**
-   * {@inheritdoc}
+   * Sets a new session variable.
+   *
+   * @param string $key
+   *   The session variable key.
+   * @param mixed $value
+   *   The session variable value.
    */
   public function set($key, $value) {
     $this->session->set($this->getSessionPrefix() . $key, $value);
@@ -52,6 +54,16 @@ class FacebookAuthPersistentDataHandler implements PersistentDataInterface {
    */
   public function getSessionPrefix() {
     return $this->sessionPrefix;
+  }
+
+  /**
+   * Sets the session prefix for the data handler.
+   *
+   * @param string $prefix
+   *   The session prefix.
+   */
+  public function setSessionPrefix($prefix) {
+    $this->sessionPrefix = $prefix . '_';
   }
 
 }
--- docroot/modules/contrib/social_auth/src/AuthManager/OAuth2Manager.php
+++ docroot/modules/contrib/social_auth/src/AuthManager/OAuth2Manager.php
@@ -2,55 +2,90 @@
 
 namespace Drupal\social_auth\AuthManager;
 
+use Drupal\Core\Config\Config;
+use Drupal\social_api\AuthManager\OAuth2Manager as BaseOAuth2Manager;
+
 /**
  * Defines a basic OAuth2Manager.
  *
  * @package Drupal\social_auth
  */
-abstract class OAuth2Manager implements OAuth2ManagerInterface {
+abstract class OAuth2Manager extends BaseOAuth2Manager implements OAuth2ManagerInterface {
 
   /**
-   * The service client.
+   * Social Auth implementer settings.
    *
-   * @var mixed
+   * @var \Drupal\Core\Config\ImmutableConfig
    */
-  protected $client;
+  protected $settings;
 
   /**
-   * Access token for OAuth2 authentication.
+   * The scopes to be requested.
    *
-   * @var mixed
+   * @var string
    */
-  protected $accessToken;
+  protected $scopes;
 
   /**
-   * {@inheritdoc}
+   * The end points to be requested.
+   *
+   * @var string
    */
-  public function setClient($client) {
-    $this->client = $client;
-    return $this;
+  protected $endPoints;
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\Core\Config\Config $settings
+   *   The implementer settings.
+   */
+  public function __construct(Config $settings) {
+    $this->settings = $settings;
+    $this->endPoints = $this->scopes = FALSE;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getClient() {
-    return $this->client;
+  public function getExtraDetails() {
+    $endpoints = $this->getEndPoints();
+
+    // Store the data mapped with endpoints define in settings.
+    $data = [];
+
+    if ($endpoints) {
+      // Iterate through api calls define in settings and retrieve them.
+      foreach (explode(PHP_EOL, $endpoints) as $endpoint) {
+        // Endpoint is set as path/to/endpoint|name.
+        $parts = explode('|', $endpoint);
+        $call[$parts[1]] = $this->requestEndPoint($parts[0]);
+        array_push($data, $call);
+      }
+
+      return json_encode($data);
+    }
+
+    return NULL;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getAccessToken() {
-    return $this->accessToken;
+  public function getScopes() {
+    if ($this->scopes === FALSE) {
+      $this->scopes = $this->settings->get('scopes');
+    }
+    return $this->scopes;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function setAccessToken($access_token) {
-    $this->accessToken = $access_token;
-    return $this;
+  public function getEndPoints() {
+    if ($this->endPoints === FALSE) {
+      $this->endPoints = $this->settings->get('endpoints');
+    }
+    return $this->endPoints;
   }
 
 }
