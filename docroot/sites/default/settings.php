9b2992f485766d8e07bb359bee8e00944809ff02
Chris Brody <chris@cbrody.com>
2018-10-09 09:48:11 +0100

    Re-add fixed search settings

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -777,7 +777,9 @@ if (isset($_SERVER['DEVDESKTOP_DRUPAL_SETTINGS_DIR']) && file_exists($_SERVER['D
 }
 // </DDSETTINGS>
 
-//$settings['acquia_search']['settings']['disable_auto_read_only'] = TRUE
+if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
+    $config['acquia_search.settings']['disable_auto_read_only'] = TRUE;
+ }
 
 /**
  * Load local development override configuration, if available.
