1634f61f55e96c93eaf27c5479c3e7976f2b1aec
Chris Brody <chris.brody@iied.org>
2022-06-27 17:45:30 +0100

    Update core to v9.4.1, all modules, config

--- docroot/modules/contrib/geofield_map/js/geolocation.js
+++ docroot/modules/contrib/geofield_map/js/geolocation.js
@@ -10,45 +10,54 @@
   Drupal.behaviors.geofieldMapGeolocation = {
     attach: function (context, settings) {
 
-      let fields = $(context);
+      let first_geofield_map = 0;
 
-      // Don't do anything if we're on field configuration.
-      if (!fields.find('#edit-instance').length) {
-
-        // Check that we have something to fill up
-        // On multi values check only that the first one is empty.
-        if (fields.find('.auto-geocode .geofield-lat').val() === '' && $fields.find('.auto-geocode .geofield-lon').val() === '') {
-
-          // Check to see if we have geolocation support, either natively or through Google.
-          if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(updateLocation, errorUpdateLocation);
-          }
-        }
+      // Trigger the HTML5 Geocoding only if not in the Geofield Field
+      // configuration page.
+      if ($(context).find('#edit-field-geofield-wrapper').length && navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(updateLocation, errorUpdateLocation);
       }
 
-      $('input[name="geofield-html5-geocode-button"]').once('geofield_geolocation').click(function (e) {
-        e.preventDefault();
-
-        fields = $(this).parents('.auto-geocode').parent();
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(updateLocation, errorUpdateLocation);
-        }
-      });
-
-      // Success callback for getCurrentPosition.
+      // Update Location for each Geofield Map based on the HTML5 Geolocation
+      // Position of the user.
       function updateLocation(position) {
-        fields.find('.auto-geocode .geofield-lat').val(position.coords.latitude.toFixed(6)).trigger('change');
-        fields.find('.auto-geocode .geofield-lon').val(position.coords.longitude.toFixed(6)).trigger('change');
+
+        const geofield_maps_array = Object.entries(settings['geofield_map']).reverse();
+        for (const [mapid, options] of geofield_maps_array) {
+          if (mapid.includes('-0-value')) {
+            first_geofield_map = 1;
+          }
+          else {
+            first_geofield_map = 0;
+          }
+          if (options.geolocation) {
+            updateGeoLocationFields($('#' + mapid, context).parents('.geofieldmap-widget-auto-geocode'), position, options);
+          }
+        }
+
+        // Bind the "updateGeoLocationFields" click eent to the
+        // "geofield-html5-geocode-button" button.
+        $('input[name="geofield-html5-geocode-button"]').once('geofield_geolocation').click(function (e) {
+          e.preventDefault();
+          updateGeoLocationFields($(this).parents('.geofieldmap-widget-auto-geocode').parent(), position, []);
+        });
+
+      }
+
+      // Update Geolocation Fields based on the user position,.
+      function updateGeoLocationFields(fields, position, options) {
+        if (options.length === 0 || (options['lat'] === 0 && options['lng'] === 0 && first_geofield_map === 1)) {
+          fields.find('.geofield-lat').val(position.coords.latitude.toFixed(6)).trigger('change');
+          fields.find('.geofield-lon').val(position.coords.longitude.toFixed(6)).trigger('change');
+        }
       }
 
       // Error callback for getCurrentPosition.
-      function errorUpdateLocation(position) {
+      function errorUpdateLocation() {
         /* eslint-disable no-console */
-        console.log('didn\'t find any HTML5 position');
+        console.log('Couldn\'t find any HTML5 position');
         /* eslint-enable no-console */
       }
-
     }
   };
-
 })(jQuery, Drupal);
